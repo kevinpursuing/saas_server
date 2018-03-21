f5786528f061144cf00ccd548e9438e86b475845
jser-kevin <393076251@qq.com>
2018-03-21 15:06:39 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const app_router = require('./apps')
 const cms_router = require('./cms')
 const sts_router = require('./upload')
 const client_router = require('./client')
+const wechat_router = require('./wechat')
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -14,4 +15,5 @@ router.use('/api/app', app_router.routes(), app_router.allowedMethods())
 router.use('/api/cms', cms_router.routes(), cms_router.allowedMethods())
 router.use('/api/sts', sts_router.routes(), sts_router.allowedMethods())
 router.use('/api/client', client_router.routes(), client_router.allowedMethods())
+router.use('/api/wechat', wechat_router.routes(), wechat_router.allowedMethods())
 module.exports = router
